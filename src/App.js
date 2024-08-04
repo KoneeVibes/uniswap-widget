a0f6dcf2c9e756776427c1e393bb59c004dd5450
Konee <104918313+KoneeVibes@users.noreply.github.com>
2024-08-04 03:29:14 +0100

    updated app.js with code for uniswap widget and initialized and populated config-overrides.js with new config for CRA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { SwapWidget } from '@uniswap/widgets';
+import '@uniswap/widgets/fonts.css';
+import { ethers } from 'ethers';
+import detectEthereumProvider from '@metamask/detect-provider';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const API_KEY = "API_KEY";
+  const jsonRPCEndpoint = `https://mainnet.infura.io/v3/${API_KEY}`;
+  const jsonRpcProvider = new ethers.providers.JsonRpcProvider(jsonRPCEndpoint);
+  const [account, setAccount] = useState({
+    address: '',
+    provider: jsonRpcProvider,
+  });
+  const tokenList = "https://ipfs.io/ipns/tokens.uniswap.org"
+
+  useEffect(() => {
+    async function connectWallet() {
+      const ethereumProvider = await detectEthereumProvider();
+      if (ethereumProvider && ethereumProvider.request) {
+        await ethereumProvider.request({ method: 'eth_requestAccounts' });
+        const provider = new ethers.providers.Web3Provider(ethereumProvider);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount({
+          address: address,
+          provider: provider,
+        });
+      } else {
+        console.error('Please install MetaMask!');
+      }
+    }
+    connectWallet();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SwapWidget
+        provider={account.provider}
+        tokenList={tokenList}
+      />
     </div>
   );
 }
